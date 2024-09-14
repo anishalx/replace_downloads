aca207ed8c5b45f56abb2b81773d4c16adfc4137
Anish <113669810+anishalx@users.noreply.github.com>
2024-09-15 02:10:11 +0530

    Update replace_downloads.py

--- tools/replace_downloads.py
+++ tools/replace_downloads.py
@@ -1,1 +1,31 @@
+#!/usr/bin/env python
+
+# Requirements
+# sudo apt-get update
+# sudo apt-get install libnetfilter-queue-dev
+# pip install netfilterqueue
+
+
+
+import netfilterqueue
+import scapy.all as scapy
+
+
+def process_packet(packet):
+    scapy_packet = scapy.IP(packet.get_payload())
+    if scapy_packet.haslayer(scapy.RAW):
+        if scapy_packet[scapy.TCP].dport == 80:
+            print("HTTP request")
+            print(scapy_packet.show())
+        elif scapy_packet[scapy.TCP].sport ==80:
+                print("HTTP response")
+                print(scapy_packet.show())
+    
+
+    packet.accept()
+
+
+queue = netfilterqueue.NetfilterQueue()
+queue.bind(0, process_packet)
+queue.run()
 
